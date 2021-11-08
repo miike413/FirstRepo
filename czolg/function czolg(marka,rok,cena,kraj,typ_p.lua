cb76736b188af3bf88838b3f193786c1018ce44b
Michał Radzicki <MicRadz@Pracownia.com>
2021-11-08 11:16:36 +0100

    xd nie pamietam

---
+++ czolg/function czolg(marka,rok,cena,kraj,typ_p.lua
@@ -0,0 +1,42 @@
+<html><head><script>
+function czolg(marka,rok,cena,kraj,typ_pancerza,stabilizator,kaliber,wersja)
+{
+    this.marka= marka;
+    this.rok=rok;
+    this.cena=cena;
+    this.kraj=kraj;
+    this.typ_pancerza=typ_pancerza;
+    this.stabilizator=stabilizator;
+    this.kaliber=kaliber;
+    this.wersja=wersja;
+}
+{
+rosja = new czolg("Rosyjski sh1t", 1970, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+putin = new czolg ("Rosyjski sh1t", 1911, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+moskwa = new czolg("Rosyjski sh1t", 1936, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+tyton = new czolg("Rosyjski sh1t", 1931, 1000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+papieros = new czolg("Rosyjski sh1t", 1954, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+fajka = new czolg("Rosyjski sh1t", 1969, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+mala = new czolg("Rosyjski sh1t", 1972, 20000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+praktyki = new czolg("Rosyjski sh1t", 1940, 3000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+filip = new czolg("Rosyjski sh1t", 1324, 3000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+good  = new czolg("Rosyjski sh1t", 1231, 000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+niemiec  = new czolg("Rosyjski sh1t", 1942, 2000000, " soviet union", "rosja", "twardy", "brak", 30mm, 82 ,);
+}
+function kot(nogi,rasa,kolor,klroczu,dataurodzenia)
+    {this.nogi = nogi;
+this.rasa = rasa;
+this.kolor = kolor;
+this.klroczu = klroczu;
+this.dataurodzenia = dataurodzenia;
+}
+Rudy = new kot ("4","perski","biały","zielone", 2003)
+Siemanek = new kot ("4","sfinks","czarny","niebieski", 2023)
+Bastianek = new kot ("4","ragdoll","biały","rudy", 2013)
+
+</script>
+</html>
+<body>
+
+</body>
+</html>
